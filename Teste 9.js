a262e6e1b2c48cec306406694ffd354d7abf7ee6
Lucas Leandro <110395319+imlucasleandro@users.noreply.github.com>
2023-03-01 14:23:52 -0400

    Update Teste 9.js

--- Teste 9.js
+++ Teste 9.js
@@ -1,16 +1,8 @@
 // Lendo o vetor com os números sorteados
 const numerosSorteados = [3,8,15,22,25,30];
- /*for (let i = 0; i < 6; i++) {
-  const numero = parseInt(prompt(`Digite o ${i + 1}º número sorteado:`));
-  numerosSorteados.push(numero); 
-}*/
 
 // Lendo o vetor com a aposta do apostador
 const apostaApostador = [2,9,15,30,45,51];
-/*for (let i = 0; i < 6; i++) {
-  const numero = parseInt(prompt(`Digite o ${i + 1}º número da sua aposta:`));
-  apostaApostador.push(numero);
-} */
 
 // Verificando os acertos do apostador
 let acertos = 0;
@@ -19,6 +11,5 @@ for (let i = 0; i < 6; i++) {
     acertos++;
   }
 }
-
 // Exibindo o número de acertos do apostador
 console.log(`O apostador acertou ${acertos} números.`);
